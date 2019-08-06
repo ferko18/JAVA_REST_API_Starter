7b093a400b4cd5e724f44fc3d101d73e59f9407c
Firealem Erko <firealemg@yahoo.com>
2019-08-06 14:33:34 -0400

    spring boot project started

---
+++ src/main/java/com/ferko/taskplanner/TaskplannerApplication.java
@@ -0,0 +1,15 @@
+package com.ferko.taskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplannerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TaskplannerApplication.class, args);
+    }
+
+}
